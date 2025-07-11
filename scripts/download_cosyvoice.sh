7475098ce5658dc8e34529f1fe50d2e3e20c680e
Duc Nguyen Thanh <ngtduc693@gmail.com>
2025-07-12 02:21:42 +0700

    update

--- scripts/download_cosyvoice.sh
+++ scripts/download_cosyvoice.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -d "./models/cosyvoice-v1" ]; then
+if [ ! -d "./models/CosyVoice2-0.5B" ]; then
   echo "Downloading CosyVoice"
   git lfs install
-  git clone https://www.modelscope.cn/studios/iic/CosyVoice2-0.5B ./models/cosyvoice-v1
+  git clone https://www.modelscope.cn/studios/iic/CosyVoice2-0.5B ./models/CosyVoice2-0.5B
   echo "Download complete"
 else
   echo "CosyVoice already exists"
